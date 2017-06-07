30e177669defc46589b64d74e6cd671d750dc3dc
Sari Stissi <sestissi16@earlham.edu>
2017-06-07 10:57:05 -0400

    Create function to create list

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ function handleSubmit(ev){
     ev.preventDefault()
     const f = ev.target
     const listSection = document.querySelector('#listSection')
+    const celebName = f.celebChoice.value
+    listSection.appendChild(makeList(celebName))
 }
 
 //create function to make list item
-function makeListItem( value){
+function makeListItem(value){
     const item = document.createElement('li')
     item.textContent = value
     return item
+}
+
+//create function to make the list
+function makeList(cName){
+    const list = document.createElement('ul')
+    const item = makeListItem(cName)
+    list.appendChild(item)
+    return list
 }
