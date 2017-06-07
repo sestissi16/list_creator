a0ddbb223bffe296059c3a878f1b1e093b50ae5c
Sari Stissi <sestissi16@earlham.edu>
2017-06-07 12:10:52 -0400

    Append items to top of list and add attributes to a button

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function handleSubmit(ev){
     const f = ev.target
     const listSection = document.querySelector('#listSection')
     const celebName = f.celebChoice.value
-    debugger
-    listSection.appendChild(makeList(celebName))
+    const madeList = makeList(celebName)
+    listSection.insertBefore(madeList, listSection.firstChild)
 }
 
 //create function to make list item
@@ -21,10 +21,22 @@ function makeListItem(value){
     const item = document.createElement('li')
     item.innerHTML =`${value}`
     //add buttons to the list item
-    const favButton = document.createElement('button')
+    const favButton = document.createElement('input')
+    //create attribute for button so that I can differentiate buttons in the future
+    const typeAtt = document.createAttribute('type')
+    typeAtt.value = 'button'
+    favButton.setAttributeNode(typeAtt)
+    const clickAtt = document.createAttribute('onclick')
+    clickAtt.value.innerHTML = `${favButton_click()}`
+    favButton.setAttributeNode(clickAtt)
+
     favButton.innerHTML = `<img src="http://weknownyourdreamz.com/images/star/star-02.jpg">`
     item.appendChild(favButton)
     return item
+}
+//create a function for when the favorite button is clicked
+function favButton_click(){
+    document.body.section.div.ul.li
 }
 
 //create function to make the list
