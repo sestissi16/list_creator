2a79ec0760596f99f81b57b9b8f33ab0e2a2ec9e
Sari Stissi <sestissi16@earlham.edu>
2017-06-07 10:50:01 -0400

    Create function to make list item

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const celebForm = document.querySelector('#celebForm')
 
 //create object for each celeb
-function createcelebObj(celebname){
-    const celebObj = {}
-    celebObj.name = celebname
-}
+//function createCelebObj(celebName){
+//    const celebObj = {}
+//   celebObj.name = celebName
+//}
 
 //create function to handle the submit
 function handleSubmit(ev){
     ev.preventDefault()
     const f = ev.target
     const listSection = document.querySelector('#listSection')
+}
+
+//create function to make list item
+function makeListItem( value){
+    const item = document.createElement('li')
+    item.textContent = value
+    return item
 }
