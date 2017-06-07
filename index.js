0b832dc07b984076b65ff3ccbae4913ebfc72c8a
Sari Stissi <sestissi16@earlham.edu>
2017-06-07 10:41:29 -0400

    Add function to turn input into an object and create div for list

---
+++ index.js
@@ -0,0 +1,8 @@
+const celebForm = document.querySelector('#celebForm')
+
+//create object for each celeb
+function createcelebObj(celebname){
+    const celebObj = {}
+    celebObj.name = celebname
+}
+
